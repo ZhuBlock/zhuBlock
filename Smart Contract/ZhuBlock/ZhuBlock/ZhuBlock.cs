7b7e8f936a3eb3c4c836db0d64bad46b4fcabffb
GwendalBrossard <brossard.gwendal@gmail.com>
2018-05-06 18:35:34 +0200

    feat(SmartContract): added a simple smart contract with a PUT and GET function

---
+++ Smart Contract/ZhuBlock/ZhuBlock/ZhuBlock.cs
@@ -0,0 +1,29 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+
+public class ZhuBlock : SmartContract
+{
+    public static object Main(string operation, params object[] args)
+    {
+        switch (operation)
+        {
+            case "insertHelloWorld":
+                return insertHelloWorld();
+            case "getHelloworld":
+                return getHelloWorld();
+            default:
+                return false;
+        }
+    }
+
+    private static bool insertHelloWorld()
+    {
+        Storage.Put(Storage.CurrentContext, "Hello", "World");
+        return true;
+    }
+
+    private static byte[] getHelloWorld()
+    {
+        return Storage.Get(Storage.CurrentContext, "Hello");
+    }
+}
